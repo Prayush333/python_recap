cee6aef3e17896b43ccc58882d3f026496bae858
prayush neupane <pyprayushneupane@gmail.com>
2026-01-20 00:10:45 +0545

    i/o

---
+++ quick-rev/d-1.py
@@ -0,0 +1,20 @@
+# variables and datatype
+
+x=10  #int
+y=3.14 #float
+name="prayush" #str
+is_active=True #bool
+
+# dynamic typing
+x=5
+x="hello"
+
+#type checking
+print(type(x))
+
+
+#I/O
+
+name=input('enter name:')
+age=int(input("enter age:"))
+printf(f'Name:{name},age:{age}')
